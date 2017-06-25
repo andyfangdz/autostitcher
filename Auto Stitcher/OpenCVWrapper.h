264cb309a9906912b12dc46f351420b56cb3f5ca
Andy Fang <andyfang.dz@gmail.com>
2017-06-25 03:57:02 -0700

    add opencv boilerplate

---
+++ Auto Stitcher/OpenCVWrapper.h
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.h
+//  Auto Stitcher
+//
+//  Created by Andy Fang on 6/25/17.
+//  Copyright © 2017 Andy Fang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCVWrapper : NSObject
+- (void)isThisWorking;
+@end
---
+++ Auto Stitcher/OpenCVWrapper.mm
@@ -0,0 +1,17 @@
+//
+//  OpenCVWrapper.mm
+//  Auto Stitcher
+//
+//  Created by Andy Fang on 6/25/17.
+//  Copyright © 2017 Andy Fang. All rights reserved.
+//
+#import <opencv2/opencv.hpp>
+#import "OpenCVWrapper.h"
+
+using namespace std;
+
+@implementation OpenCVWrapper
+- (void) isThisWorking {
+    cout << "Hey this is working!" << endl;
+}
+@end
